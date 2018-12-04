0f078e5acf3c503ddd386034251462131f39fac7
shinoka7 <shinoka7@github.com>
2018-12-04 01:14:09 -0500

    Added detection starter code

---
+++ detection.py
@@ -0,0 +1,53 @@
+import numpy as np
+#import pandas as pd
+
+def rhyme(inp, level):
+     entries = nltk.corpus.cmudict.entries()
+     syllables = [(word, syl) for word, syl in entries if word == inp]
+     rhymes = []
+     for (word, syllable) in syllables:
+          rhymes += [word for word, pron in entries if pron[-level:] == syllable[-level:]]
+     return set(rhymes)
+
+def doTheyRhyme(word1, word2):
+     if word1.find(word2) == len(word1) - len(word2):
+          return False
+     if word2.find(word1) == len(word2) - len(word1): 
+          return False
+
+     return word1 in rhyme(word2, 1)
+
+def evaluate(istr):
+     istr = istr.replace(","," ").replace("."," ").replace(";"," ").replace(":"," ").replace("!"," ")
+     line = istr.splitlines()
+     i=0
+     Set = []
+     S_set = []
+     result = ""
+     for li in line:
+          S_set.append(li)
+          sp = li.split()
+          print(sp)
+          if("".join(sp[-1:]) is None or len("".join(sp[-1:])) == 0 or "".join(sp[-1:])==" "):
+               continue
+          print (str(i)+" "+"".join(sp[-1:]))
+          Set.append("".join(sp[-1:]))
+          if(len(Set)%4==0):
+               if(  doTheyRhyme(Set[len(Set)-4], Set[len(Set)-3] )==True and  doTheyRhyme(Set[len(Set)-3], Set[len(Set)-2] )==True and doTheyRhyme(Set[len(Set)-2], Set[len(Set)-1] )==True):
+                    result = "--AAAA--"
+               elif(doTheyRhyme(Set[len(Set)-4], Set[len(Set)-3] )==True and  doTheyRhyme(Set[len(Set)-3], Set[len(Set)-2] )==False and doTheyRhyme(Set[len(Set)-2], Set[len(Set)-1] )==True):
+                    result = "--AABB--"
+               elif (  doTheyRhyme(Set[len(Set)-4], Set[len(Set)-2] )==True and  doTheyRhyme(Set[len(Set)-3], Set[len(Set)-1] )==False and doTheyRhyme(Set[len(Set)-3], Set[len(Set)-2] )==False):
+                    result = "--ABAB--"
+               elif (  doTheyRhyme(Set[len(Set)-4], Set[len(Set)-1] )==True and  doTheyRhyme(Set[len(Set)-3], Set[len(Set)-1] )==False and doTheyRhyme(Set[len(Set)-3], Set[len(Set)-2] )==True):
+                    result = "--ABBA--"   
+          i=i+1
+     if(result==""):
+          result=("--No Rhyme--")
+     print(result)
+     print(S_set)
+     return
+
+if __name__ == "__main__":
+     inputstr = str(input("Enter string: "))
+     print(evaluate(inputstr))
