c174ef6fe03e70f92c59e88504c43d4d376b6d24
shinoka7 <42939055+shinoka7@users.noreply.github.com>
2018-12-12 15:55:00 -0500

    changed to multithread

--- detection.py
+++ detection.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import nltk
+from multiprocessing import Process, Lock
 
 
 def rhyme(inp, level):
@@ -14,12 +15,11 @@ def rhyme(inp, level):
 def doTheyRhyme ( word1, word2 ):
     # first, we don't want to report 'glue' and 'unglue' as rhyming words
     # those kind of rhymes are LAME
-    if word1.find ( word2 ) == len(word1) - len ( word2 ):
+    return word1 in rhyme ( word2, 1 )
+''' if word1.find ( word2 ) == len(word1) - len ( word2 ):
         return False
     if word2.find ( word1 ) == len ( word2 ) - len ( word1 ): 
-        return False
-
-    return word1 in rhyme ( word2, 1 )
+        return False'''
 
 def similes(Sentence_set):
     result = {'sen_num': None, 'alliter': None, 'repetition': None, 'simile': None}
@@ -75,16 +75,70 @@ def repetition(Sentence_set):
         return None     
     return result
 
+def rhymetype(lock, Set):
+    lock.acquire()
+    result = ""
+    if(len(Set)==4):
+        print(Set[0], Set[1],Set[2],Set[3])
+        if(  doTheyRhyme(Set[0], Set[1] )==True and doTheyRhyme(Set[1], Set[2])==True and doTheyRhyme(Set[2], Set[3] )==True):
+            result= ("--AAAA--")
+        elif (  doTheyRhyme(Set[0], Set[1] )==True and doTheyRhyme(Set[2], Set[3])==True):
+            result =("--AABB--")
+        elif (  doTheyRhyme(Set[0], Set[2] )==True and doTheyRhyme(Set[1], Set[3])==True):
+            result =("--ABAB--")
+        elif (  doTheyRhyme(Set[0], Set[3] )==True and doTheyRhyme(Set[1], Set[2])==True):
+            result =("--ABBA--")         
+    if(result==""):    
+        result= ("--No Rhyme--")
+    print(result)
+    lock.release()
+    return
+    
+
 if __name__ == "__main__":
     string = 'Let the bird of loudest lay\r\nOn the sole Arabian tree\r\nHerald sad and trumpet be,\r\nTo whose sound chaste wings obey.\r\n\r\nBut thou shrieking harbinger,\r\nFoul precurrer of the fiend,\r\nAugur of the fever\'s end,\r\nTo this troop come thou not near.\r\n\r\nFrom this session interdict\r\nEvery fowl of tyrant wing,\r\nSave the eagle, feather\'d king;\r\nKeep the obsequy so strict.\r\n\r\nLet the priest in surplice white,\r\nThat defunctive music can,\r\nBe the death-divining swan,\r\nLest the requiem lack his right.\r\n\r\nAnd thou treble-dated crow,\r\nThat thy sable gender mak\'st\r\nWith the breath thou giv\'st and tak\'st,\r\n\'Mongst our mourners shalt thou go.\r\n\r\nHere the anthem doth commence:\r\nLove and constancy is dead;\r\nPhoenix and the Turtle fled\r\nIn a mutual flame from hence.\r\n\r\nSo they lov\'d, as love in twain\r\nHad the essence but in one;\r\nTwo distincts, division none:\r\nNumber there in love was slain.\r\n\r\nHearts remote, yet not asunder;\r\nDistance and no space was seen\r\n\'Twixt this Turtle and his queen:\r\nBut in them it were a wonder.\r\n\r\nSo between them love did shine\r\nThat the Turtle saw his right\r\nFlaming in the Phoenix\' sight:\r\nEither was the other\'s mine.\r\n\r\nProperty was thus appalled\r\nThat the self was not the same;\r\nSingle nature\'s double name\r\nNeither two nor one was called.\r\n\r\nReason, in itself confounded,\r\nSaw division grow together,\r\nTo themselves yet either neither,\r\nSimple were so well compounded;\r\n\r\nThat it cried, "How true a twain\r\nSeemeth this concordant one!\r\nLove has reason, reason none,\r\nIf what parts can so remain."\r\n\r\nWhereupon it made this threne\r\nTo the Phoenix and the Dove,\r\nCo-supremes and stars of love,\r\nAs chorus to their tragic scene:\r\n\r\n                 threnos\r\n\r\nBeauty, truth, and rarity,\r\nGrace in all simplicity,\r\nHere enclos\'d, in cinders lie.\r\n\r\nDeath is now the Phoenix\' nest,\r\nAnd the Turtle\'s loyal breast\r\nTo eternity doth rest,\r\n\r\nLeaving no posterity:\r\n\'Twas not their infirmity,\r\nIt was married chastity.\r\n\r\nTruth may seem but cannot be;\r\nBeauty brag but \'tis not she;\r\nTruth and beauty buried be.\r\n\r\nTo this urn let those repair\r\nThat are either true or fair;\r\nFor these dead birds sigh a prayer.'
     
     lin = string.replace(","," ").replace("."," ")\
         .replace(";"," ").replace(":"," ").replace("!"," ")
     line = lin.splitlines()
-    Set=[]
-    Sentence_set=[]
-    result=""
+    Set = []
+    Sentence_set = []
+    four_set = []
+    result = ""
+    results = []
     
+    i = 0
+    for li in line:
+        Sentence_set.append(li)
+        sp=li.split()
+        if (not("".join(sp[-1:]) is None or  len("".join(sp[-1:])) == 0 or "".join(sp[-1:])==" ")):
+            Set.append(("".join(sp[-1:])).lower())        
+    
+    for word in Set:
+        if(i%4==0):
+            if(not(i==0)):
+                four_set.append(temp)
+            temp = []
+        temp.append(word)
+        i+=1
+    four_set.append(temp)
+    
+    lock = Lock()
+    tempo = []
+    for each in four_set:
+        Process(target=rhymetype, args=(lock, each)).start()
+        
+    print ("--sentences for similes  in first poem--")
+    print (similes(Sentence_set)) 
+    
+    print ("--sentences for alliterations  in first poem--")
+    print (alliter(Sentence_set))  
+
+    print ("--sentences for repetitions   in first poem--")
+    print (repetition(Sentence_set))    
+    
+    
+"""     
     for li in line:
         #print(li)
         Sentence_set.append(li)
@@ -96,24 +150,16 @@ if __name__ == "__main__":
         if(len(Set)%4==0):
             if(  doTheyRhyme(Set[len(Set)-4], Set[len(Set)-3] )==True and  doTheyRhyme(Set[len(Set)-3], Set[len(Set)-2] )==True and doTheyRhyme(Set[len(Set)-2], Set[len(Set)-1] )==True):
                 result= ("--AAAA--")
+                results.append(result)
             elif (  doTheyRhyme(Set[len(Set)-4], Set[len(Set)-3] )==True and  doTheyRhyme(Set[len(Set)-3], Set[len(Set)-2] )==False and doTheyRhyme(Set[len(Set)-2], Set[len(Set)-1] )==True):
                 result =("--AABB--")
+                results.append(result)
             elif (  doTheyRhyme(Set[len(Set)-4], Set[len(Set)-2] )==True and  doTheyRhyme(Set[len(Set)-3], Set[len(Set)-1] )==False and doTheyRhyme(Set[len(Set)-3], Set[len(Set)-2] )==False):
                 result =("--ABAB--")
+                results.append(result)
             elif (  doTheyRhyme(Set[len(Set)-4], Set[len(Set)-1] )==True and  doTheyRhyme(Set[len(Set)-3], Set[len(Set)-1] )==False and doTheyRhyme(Set[len(Set)-3], Set[len(Set)-2] )==True):
                 result =("--ABBA--")
+                results.append(result)
     if(result==""):    
         result= ("--No Rhyme--")
-        
-    print ("---last word of the sentence, see the below for the result ---")
-    print(result)           
-   
-    print ("--sentences for similes  in first poem--")
-    print (similes(Sentence_set)) 
-   
-    print ("--sentences for alliterations  in first poem--")
-    print (alliter(Sentence_set))  
-   
-    print ("--sentences for repetitions   in first poem--")
-    print (repetition(Sentence_set))
-                
+        results.append(result)"""               
